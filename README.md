8739d235318fe594b72c7fada7c9ce4739c141dc
Nick Meinhold <nick@enspyr.co>
2023-04-30 14:24:03 +1000

    Use oak middleware, re-arrange for findability
    I used Application & Router from oak to serve files and
    to return the cotents of index.html from /

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 A very basic web IDE powered by the [Monaco Editor](https://microsoft.github.io/monaco-editor/), served from a
 [Deno](https://deno.com) web server.
 
+[Project Notes](https://www.notion.so/enspyrco/Embeddable-Editor-cb51e09f892d4e76b40fa5a2671a055f)
+
 ## Run
 
 ```sh
@@ -16,3 +18,24 @@ The code was originally adapted from:
 [aleph.js > examples > manoco-editor](https://github.com/alephjs/aleph.js/tree/1.0.0-alpha.52/examples/feature-apps/monaco-editor)
 
 > [Licence = MIT](https://github.com/alephjs/aleph.js/blob/1.0.0-alpha.52/LICENSE)
+
+## Strategies for transpiling ts and bundling
+
+### aleph.js
+
+I followed the [instructions](https://github.com/alephjs/aleph.js/tree/1.0.0-alpha.52) but kept getting errors (I didn't make any notes unfortunately).
+
+### esbuild
+
+```sh
+npm install --save-exact esbuild
+./node_modules/.bin/esbuild src/main.ts --bundle --outfile=dist/main.js
+```
+
+I get an error about:
+
+> Dynamic require of "..." is not supported
+
+### webpack
+
+Had a quick play, seems complicated to both transpile ts and bundle…
--- server.ts
+++ server.ts
@@ -1,18 +1,78 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
 const htmlFile = await Deno.readFile('./index.html');
 const decoder = new TextDecoder();
 const decodedHtml = decoder.decode(htmlFile);
 
-const server = Deno.listen({ port: 8080 });
-console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
+const app = new Application();
+const port = 8080;
 
-for await (const conn of server) {
-  handleHttp(conn).catch(console.error);
-}
-
-async function handleHttp(conn: Deno.Conn) {
-  const httpConn = Deno.serveHttp(conn);
-  for await (const requestEvent of httpConn) {
-    const response = new Response(decodedHtml, {headers: {'Content-Type': 'text/html'}});
-    await requestEvent.respondWith(response);
+app.use(async (context, next) => {
+  try {
+    await context.send({
+      root: `${Deno.cwd()}`,
+      index: "index.html",
+    });
+  } catch {
+    await next();
   }
-}
+});
+
+const router = new Router();
+
+router.get("/", async (ctx) => {
+  ctx.response.body = decodedHtml;
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("Listening at http://localhost:" + port);
+await app.listen({ port });
+
+
+///////
+
+// const htmlFile = await Deno.readFile('./index.html');
+// const decoder = new TextDecoder();
+// const decodedHtml = decoder.decode(htmlFile);
+
+// const server = Deno.listen({ port: 8080 });
+// console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
+
+// for await (const conn of server) {
+//   handleHttp(conn).catch(console.error);
+// }
+
+// async function handleHttp(conn: Deno.Conn) {
+//   const httpConn = Deno.serveHttp(conn);
+//   for await (const requestEvent of httpConn) {
+//     // Use the request pathname as filepath
+//     const url = new URL(requestEvent.request.url);
+//     const filepath = decodeURIComponent(url.pathname);
+
+//     // Try opening the file
+//     let file;
+//     try {
+//       file = await Deno.open("." + filepath, { read: true });
+//     } catch {
+//       // If the file cannot be opened, return a "404 Not Found" response
+//       const notFoundResponse = new Response("404 Not Found", { status: 404 });
+//       await requestEvent.respondWith(notFoundResponse);
+//       continue;
+//     }
+
+//     // Build a readable stream so the file doesn't have to be fully loaded into
+//     // memory while we send it
+//     const readableStream = file.readable;
+
+//     // Build and send the response
+//     const response = new Response(readableStream);
+//     await requestEvent.respondWith(response);
+//   }
+// }
+
+//////////
+
+// const response = new Response(decodedHtml, {headers: {'Content-Type': 'text/html'}});
+//     await requestEvent.respondWith(response);
